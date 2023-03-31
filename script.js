08096474895471d1a9dcf86795958e9f30465f96
Aihhhhhh <mwanikidenno2@gmail.com>
2023-03-31 15:40:06 +0300

    create a javascript function for creating the grid

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const div = document.getElementById("divContainer");
+function squareGrid(dimension){
+    const cdiv = document.getElementById("divContainer");
+    const total = (dimension *dimension) + dimension;
+    const mod = dimension +1;
+
+    for(let i =0; i<total; i++){
+        const div = document.createElement("div");
+
+        if(i % mod === 0){
+            div.style.cssText = "border:0 , height:0, width:100%"
+        }else{
+            div.style.cssText = "border:1px solid black, height: 40px, width :40px";
+        }
+        cdiv.appendChild(div);
+    }
+}
+squareGrid(16);
+
+
 
