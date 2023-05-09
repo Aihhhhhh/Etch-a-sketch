d1fc3aff74e403700b6f226cdc53d5dd9e177065
Aihhhhhh <mwanikidenno2@gmail.com>
2023-05-09 13:31:46 +0300

    create a function to handle the user inputs using the window prompt method and add an event listener to the button which listens for the click event and fires the prompts

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+//selecting the button using its class selector
+const button = document.querySelector(".popup");
+//Adding  text content to the button
+button.textContent = "Click here";
+/*create a function for the prompts
+Add an event listener that looks out for the click*/
+
+const userAns = ()=>{
+    let ans1 = prompt("Please enter the number of squares for one  side", "Grid dimensions for one side");
+    let firstSide = parseInt(ans1);
+    let ans2  = prompt("Please enter the number of squares for the other side", "Grid dimensions for the other side");
+    let secondSide = parseInt(ans2);
+    }
+
+button.addEventListener("click", userAns)
+
 //creating a sqaue grid
 var n = 16;
-
+//selecting the div container which holds the entire grid
 const cDiv = document.querySelector(".divContainer");
 
-
+//creating the div
 for(let i =1; i<n; i++){
     cDiv.innerHTML+= '<div class = "row">';
 
@@ -17,18 +33,8 @@ const smallBoxes = document.querySelectorAll(".smallBox");
 [...smallBoxes].forEach(smallBox=>{
     smallBox.addEventListener("mouseover", ()=> {
         smallBox.classList.add("permahover");
-    })
+    });
 });
-
-
-
-
-
-
-
-
-
-
 
 
 
